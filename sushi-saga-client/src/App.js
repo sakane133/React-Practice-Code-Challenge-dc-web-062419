a80d279ab9fd6a294e519428f761e9995c8d7dcb
github username <github email address>
2019-09-02 22:34:34 -0400

    can eat and get more sushi

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,51 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    sushiIndex: 0
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(sushiArr => {
+      let sushiInfo =  sushiArr.map(sushi =>{
+        sushi.eaten = false
+        return sushi })
+      this.setState({sushis: sushiInfo})  
+        })
+      }
+
+   showFour = () => {
+     return this.state.sushis.slice(this.state.sushiIndex, this.state.sushiIndex + 4)
+    }
+
+    showMoreButt = () => {
+      return this.setState({sushiIndex: this.state.sushiIndex + 4})
+    }
+
+    eatSushi = (id) => {
+   
+      let newSush = this.state.sushis.map(sushi => {
+        if(id === sushi.id){
+          sushi.eaten = true
+          return sushi
+         
+        }
+        else {
+          return sushi
+        }
+      })
+      this.setState({sushis: newSush})
+    }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer  sushis={this.showFour()}
+           moreButton={this.showMoreButt}
+           eatSushi={this.eatSushi}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
@@ -9,8 +10,9 @@ const SushiContainer = (props) => {
           /* 
              Render Sushi components here!
           */
+         props.sushis.map(sushi => <Sushi key={sushi.id} sushi={sushi} eatSushi={props.eatSushi}/>)
         }
-        <MoreButton />
+        <MoreButton moreButton={props.moreButton}/>
       </div>
     </Fragment>
   )
